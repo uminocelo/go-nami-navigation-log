71f9afd53ae55387d3fe708ed4864b0a4d90bff0
uminocelo <marcelo.mdl.dev@gmail.com>
2023-03-12 21:27:26 -0300

    login

--- models/user.go
+++ models/user.go
@@ -1,10 +1,51 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"html"
+	"nami_navigation_log/database"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Entries  []Entry
+}
+
+func (user *User) Save() (*User, error) {
+	err := database.Database.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+
+	return user, nil
+}
+
+func (user *User) BeforeSave(*gorm.DB) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(passwordHash)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	return nil
+}
+
+func (user *User) ValidatePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+}
+
+func FindUserByUsername(username string) (User, error) {
+	var user User
+	err := database.Database.Where("username=?", username).Find(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
